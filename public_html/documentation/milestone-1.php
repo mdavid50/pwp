948cc0124601bb730a16ae885c82bfa5709dd56a
Matt David <mcdav3636@gmail.com>
2017-10-13 11:06:41 -0600

    Adding/edit persona

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -17,6 +17,18 @@
 
 		<h2>Persona</h2>.
 
-		<p>Mrs. Maher is the hiring manager for a development company. </p>
+		<p><strong>Name:</strong> Mrs. Maher</p>
+
+		<p><strong>Age:</strong>39</p>
+
+		<p><strong> Profession:</strong>Hiring manager at ___________, a developement company looking to expand
+		and hire developers with a solid foundation that will grow with the company.</p>
+
+		<p><strong>Technologies:</strong> Uses HP</p>
+
+		<p>Mrs. Maher is the hiring manager for a well established development company. Her company is expanding and
+			she is looking for a backend developers with a solid and broad foundation to join her teams outstanding
+			atmosphere. She seeks great communication and a strong work ethic because her company believes in the
+			freedom to work remote.</p>
 	</body>
 </html>
