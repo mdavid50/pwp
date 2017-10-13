e6c4988853bb6f58ee82f342f28ed0ea05186302
Matt David <mcdav3636@gmail.com>
2017-10-13 11:35:25 -0600

    added to persona starting at attitude and behaviors

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -24,11 +24,26 @@
 		<p><strong> Profession:</strong>Hiring manager at ___________, a developement company looking to expand
 		and hire developers with a solid foundation that will grow with the company.</p>
 
-		<p><strong>Technologies:</strong> Uses HP</p>
+		<p><strong>Technologies:</strong> Uses HP 15-ay013nr and uses a Motorola Droid Turbo</p>
 
-		<p>Mrs. Maher is the hiring manager for a well established development company. Her company is expanding and
-			she is looking for a backend developers with a solid and broad foundation to join her teams outstanding
-			atmosphere. She seeks great communication and a strong work ethic because her company believes in the
-			freedom to work remote.</p>
+		<p><strong>Attitude and Behaviors:</strong> Has been doing her job for 10 years and knows what she is looking for.
+		Doesn't spend much time on frill and wants the facts and evidence. She is polite but assertive and usually gets
+		what she wants.</p>
+
+		<p><strong>Frustrations and Needs:</strong> Mrs. Maher Mrs. Maher is the hiring manager for a well established
+			development company. Her company is expanding and she is looking for a backend developers with a solid and
+			broad foundation to join her teams outstanding atmosphere. She seeks great communication and a strong work
+			ethic because her company believes in the freedom to work remote. Mrs. Maher wants to see what a
+			potential employee is capable of quickly through their experience and GitHub.</p>
+
+		<p><strong>Goal:</strong> Mrs. Maher wants to find her qualified candidates quickly because her company has
+		taken on new contracts with eager customers. She wants skim through her 326 applicants to see experience and
+		a GitHub pulse to narrow her field and start interviewing.</p>
+
+		<p><strong>User Story:</strong> As a user I need to quickly see what experience a candidate has and
+		have a quick reference to their recent coding activity on GitHub.</p>
+
+
+		<p></p>
 	</body>
 </html>
