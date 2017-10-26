45ddb01aae9a76df633d3c8de959699c08302da1
Matt David <mcdav3636@gmail.com>
2017-10-26 11:34:45 -0600

    edited alt att

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -17,7 +17,7 @@
 
 		<h2>Persona</h2>.
 		
-		<img src="../images/business-woman.jpg" alt="photo of mrs. maher">
+		<img src="../images/business-woman.jpg" alt="Photo of Mrs. Maher">
 
 		<p><strong>Name:</strong> Mrs. Maher</p>
 
