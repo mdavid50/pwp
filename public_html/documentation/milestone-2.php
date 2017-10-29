1693b14cf699116cf691c47b48546c8d840adcbe
Matt David <mcdav3636@gmail.com>
2017-10-29 12:47:07 -0600

    added file milestone-2

---
+++ public_html/documentation/milestone-2.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+    <html>
+    <head>
+        <meta charset="UTF-8">
+        <title>
+          Whats to come
+        </title>
+    </head>
+    <body>
+    <h1>Whats to come</h1>
+    <p>I don't want to start my relationship with Mrs. Maher off on a bad not. Keeping in mind that she has a lot
+    to do today, I want to present her with a strong but simple page. I'm going to do my best to give her better
+    than the basics, but get to the point right away. Here is what I think will be important to Mrs. Maher.</p>
+    <ul>
+        <li>background that includes my name that is not busy. It presents a warm feel that makes you
+            appreciate open space</li>
+        <li>navigation buttons to lead you to desired areas of the site. tabs will include who am I, capabilities,
+        skills, resume, and contact.</li>
+        <li>small who am I (about me section)</li>
+        <li>capabilities section describing languages with experience</li>
+        <li>skills section describing professional skills outside languages</li>
+        <li>resume</li>
+        <li>footer with contacts and links</li>
+    </ul>
+    </body>
+    </html>
