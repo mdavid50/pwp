6b63d3d134f15a6ef56ba2e2aad54e423373e79a
Matt David <mcdav3636@gmail.com>
2017-11-15 15:37:41 -0700

    created an index.php

---
+++ public_html/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Matthew J David</title>
+    <!-- required meta tags -->
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- link to css stylesheet -->
+    <link rel="stylesheet" href="style/style.css" type="text/css"/>
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+
+    <!-- java script link -->
+    <script src="style/script.js"></script>
+
+</head>
+
+<body class="sfooter home">
+<div class="sfooter content">
+    <!-- insert the header and nav -->
+    <nav class="navbar navbar-fixed-top navbar-default">
+        ::before
+        <div class="container"></div>
+        ::before
+    </nav>
+</div>
+</body>
+</html>
