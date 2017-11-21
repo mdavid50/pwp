831d19cb4ba2270cd3e3fd1fa92b296ee02b510a
mdavid5 <mcdav3636@gmail.com>
2017-11-21 16:42:22 -0700

    added structure and background

--- public_html/index.php
+++ public_html/index.php
@@ -13,17 +13,14 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
 
     <!-- java script link -->
-    <script src="style/script.js"></script>
+    <script src="scripts/main.js"></script>
 
 </head>
 
-<body class="sfooter home-layout parallax">
-<div class="sfooter content">
-
-
+<body>
 
 	<!-- navigation bar -->
-	<nav class="navbar navbar-default navbar-expand-md navbar-dark bg-dark" id="top-of-page">
+	<nav class="navbar fixed-top navbar-default navbar-expand-md navbar-dark bg-dark" id="top-of-page">
 		<a class="navbar-brand" href="index.php">Matthew David</a>
 		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
 			<span class="navbar-toggler-icon"></span>
@@ -56,19 +53,15 @@
 	</nav>
 
 	<!--parallax first section -->
-	<div class="parallax-content">
-		<div class="parallax"></div>
-	</div>
-
-	<!-- welcome 'hero' section -->
-	<section class="welcome p-t-nav">
-		<div class="container">
+	<section class="parallax-content">
+		<div class="parallax">
 			<div class="jumbotron bg-transparent text-center">
 				<h1>Glad you could make it!</h1>
 				<p class="lead">Thanks for taking the time to check out what I'm capable of!</p>
 			</div>
 		</div>
-	</section><!--/.welcome-->
+	</section>
+
 
 	<!-- about Matt David -->
 	<section class="p-5 solid mt-0" id="about">
@@ -84,9 +77,43 @@
 
 	<!-- parallax second section -->
 	<div class="parallax-content2">
-		<div class="parallax"></div>
+		<div class="parallax">
+			<div class="jumbotron bg-transparent float-left">
+				<h1>Languages I have learned</h1>
+			</div>
+		</div>
 	</div>
 
-	<section class=""
-</div>
+	<!-- Languages I know -->
+	<section class="p-5 solid mt-0" id="languages">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-8 col-lr-6">
+					<h2>I can talk the talk</h2>
+					<p>I have learned all these things while developing</p>
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<!-- parallax third section -->
+	<div class="parallax-content3">
+		<div class="parallax">
+			<div class="jumbotron bg-transparent float-right">
+				<h1>My Other Attributes</h1>
+				<p>Soft skills gathered over time</p>
+			</div>
+		</div>
+	</div>
+
+	<section class="p-5 solid mt-0" id="skills">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-8 col-lr-6">
+					<h2>Skills</h2>
+					<p>Slacking, Sleeping, eating, bbbrrrraaaaappppppp</p>
+				</div>
+			</div>
+		</div>
+	</section>
 </body>
